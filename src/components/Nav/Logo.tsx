c44d07e30a9d07730e2f46cd1911a7f414598111
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2025-03-07 14:04:05 -0300

    Ajuste en Nav y responsive

--- src/components/Nav/Logo.tsx
+++ src/components/Nav/Logo.tsx
@@ -1,8 +1,8 @@
 const Logo = () => {
     return(
-        <div className="w-auto h-full ml-10 text-white flex flex-col  justify-center items-center select-none">
+        <div className="w-auto h-full text-white flex flex-col  justify-center items-center select-none sm:ml-10 md:ml:15 lg:ml-20">
             <h1 className="w-full extrabold text-3xl text-center ">POMODORO</h1>
-            <div className="w-full light text-xl flex flex-row justify-center items-center gap-3">
+            <div className="w-full light text-xl flex flex-row justify-between items-center gap-3">
                 <h2>🍅</h2>
                 <h2 className="tracking-[.5em]">TIMER</h2>
                 <h2>⏰</h2>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,9 +3,9 @@ import Menu from "../../assets/menu-white.svg"
 
 const Nav = () =>{
     return(
-        <div className="h-20 mt-8 flex flex-row justify-between sm:text-xl">
+        <div className="h-20 mt-10 flex flex-row justify-between sm:text-xl">
             <Logo/>
-            <div className="w-14 mr-10 flex justify-center items-center">
+            <div className="w-14 flex justify-center items-center sm:mr-10 md:mr:15 lg:mr-20">
                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
                     <img className="h-16 w-auto" src={Menu} alt="Menu" />
                 </a>
