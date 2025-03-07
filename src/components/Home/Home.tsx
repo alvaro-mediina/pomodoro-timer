6423ad86ec89918e5dbe8ebe919a71dbc02c9d91
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2025-03-07 17:39:04 -0300

    Tomate 3D

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,12 @@
 import Nav from "../Nav/Nav";
+import Tomato3D from "./Tomato3D";
 
 function Home () {
     return(
         <div className="min-h-screen bg-background flex flex-col overflow-x-hidden overflow-y-auto">
             <Nav/>
-            <main className="flex-grow">
-
+            <main className="flex flex-grow items-center justify-center">
+            <Tomato3D/> 
             </main>
         </div>
     );
---
+++ src/components/Home/Tomato3D.tsx
@@ -0,0 +1,49 @@
+import { useRef, useEffect, useState } from 'react';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+
+const TomatoModel = () => {
+  const gltf = useLoader(GLTFLoader, '/model/tomate-model.glb');
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <primitive
+      ref={meshRef}
+      object={gltf.scene}
+      scale={[100, 100, 100]}
+      position={[0, 30, 0]}
+    />
+  );
+};
+
+const Tomato3D = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 100);
+  }, []);
+
+  return (
+    <Canvas
+      style={{ width: '100%', height: '500px' }}
+      camera={{ position: [0, 0, 320], fov:30}}
+    >
+      <ambientLight intensity={1.4} />
+      <pointLight position={[2, 2, 2]} />
+      {isLoaded && <TomatoModel />}
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
+};
+
+export default Tomato3D;
