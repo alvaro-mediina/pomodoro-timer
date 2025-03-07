d6f82cde2a370a444f98f10e1a0fd3de43e0e987
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2025-03-07 14:06:10 -0300

    ajuste en el responsive

--- src/components/Nav/Logo.tsx
+++ src/components/Nav/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
     return(
-        <div className="w-auto h-full text-white flex flex-col  justify-center items-center select-none sm:ml-10 md:ml:15 lg:ml-20">
+        <div className="w-auto h-full ml-7 text-white flex flex-col  justify-center items-center select-none sm:ml-10 md:ml:15 lg:ml-20">
             <h1 className="w-full extrabold text-3xl text-center ">POMODORO</h1>
             <div className="w-full light text-xl flex flex-row justify-between items-center gap-3">
                 <h2>🍅</h2>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = () =>{
     return(
         <div className="h-20 mt-10 flex flex-row justify-between sm:text-xl">
             <Logo/>
-            <div className="w-14 flex justify-center items-center sm:mr-10 md:mr:15 lg:mr-20">
+            <div className="w-14 mr-7 flex justify-center items-center sm:mr-10 md:mr:15 lg:mr-20">
                 <a href="https://example.com" target="_blank" rel="noopener noreferrer">
                     <img className="h-16 w-auto" src={Menu} alt="Menu" />
                 </a>
