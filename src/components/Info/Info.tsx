9412526b07771342a71b72ef7603beae537b4de8
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2025-03-09 22:18:03 -0300

    Apartado para Info, Constantes y componente Card

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,7 +1,44 @@
+import { Card } from '../../models/utils-models'
+import Fatiga from '../../assets/medium-shot-guy-expressing-victory.jpg'
+import Gestion from '../../assets/defocused-smiley-woman-holding-light-bulb-clock.jpg'
+import Memoria from '../../assets/young-man-writing-notebook-study-session.jpg'
+import InfoCardGrid from './InfoCardGrid'
+import {
+        FATIGA_TITLE, 
+        FATIGA_DESC,
+        GESTION_TITLE,
+        GESTION_DESC,
+        MEMORIA_TITLE,
+        MEMORIA_DESC
+    } from "../../utils/Constants";
+
+
+const DataCards: Card[] = [
+    {    
+        id: 1,
+        title: FATIGA_TITLE,
+        description: FATIGA_DESC,
+        img: Fatiga,
+    },
+    {
+        id: 2,
+        title: GESTION_TITLE,
+        description: GESTION_DESC,
+        img: Gestion,
+    },
+    {
+        id: 3,
+        title: MEMORIA_TITLE,
+        description: MEMORIA_DESC,
+        img: Memoria,
+    }
+];
+
+
 function Info(){
     return(
         <div className="bg-white min-h-screen">
-
+            <InfoCardGrid infoCards={ DataCards } />
         </div>
     )
 }
---
+++ src/components/Info/InfoCardGrid.tsx
@@ -0,0 +1,15 @@
+import { Card } from '../../models/utils-models'
+
+interface Props {
+    infoCards: Card[];
+}
+
+const InfoCardGrid = ({ infoCards } : Props) => {
+    return(
+        <div>
+ 
+        </div>
+    );
+};
+
+export default InfoCardGrid;
