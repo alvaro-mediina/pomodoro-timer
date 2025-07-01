f69ab14c23c497adc9a8e537eca2baa8b6c24b72
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-06-30 21:32:27 -0300

    refactor: :recycle: Refactorización de las cartas de información a constantes

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,38 +1,5 @@
-import { Card } from '../../models/utils-models'
-import Fatiga from '../../assets/medium-shot-guy-expressing-victory.jpg'
-import Gestion from '../../assets/defocused-smiley-woman-holding-light-bulb-clock.jpg'
-import Memoria from '../../assets/young-man-writing-notebook-study-session.jpg'
 import InfoCardGrid from './InfoCardGrid'
-import {
-        FATIGA_TITLE, 
-        FATIGA_DESC,
-        GESTION_TITLE,
-        GESTION_DESC,
-        MEMORIA_TITLE,
-        MEMORIA_DESC
-    } from "../../utils/Constants";
-
-
-const DataCards: Card[] = [
-    {    
-        id: 1,
-        title: FATIGA_TITLE,
-        description: FATIGA_DESC,
-        img: Fatiga,
-    },
-    {
-        id: 2,
-        title: GESTION_TITLE,
-        description: GESTION_DESC,
-        img: Gestion,
-    },
-    {
-        id: 3,
-        title: MEMORIA_TITLE,
-        description: MEMORIA_DESC,
-        img: Memoria,
-    }
-];
+import { DataCards } from "../../utils/Constants";
 
 
 function Info(){
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,3 +1,8 @@
+import { Card } from '../models/utils-models'
+import Fatiga from '../assets/medium-shot-guy-expressing-victory.jpg'
+import Gestion from '../assets/defocused-smiley-woman-holding-light-bulb-clock.jpg'
+import Memoria from '../assets/young-man-writing-notebook-study-session.jpg'
+
 export const FATIGA_TITLE = "😮‍💨 Evita la fatiga mental";
 export const FATIGA_DESC = "Trabajar en bloques de tiempo con pausas regulares previene el agotamiento y mejora el enfoque a largo plazo.";
 
@@ -6,3 +11,25 @@ export const GESTION_DESC = "Saber que tienes un tiempo limitado para trabajar e
 
 export const MEMORIA_TITLE = "😎 Favorece a la memoria y al aprendizaje";
 export const MEMORIA_DESC = "Trabajar en bloques de tiempo con pausas regulares previene el agotamiento y mejora el enfoque a largo plazo.";
+
+// Cartas de Información INICIO
+export const DataCards: Card[] = [
+    {    
+        id: 1,
+        title: FATIGA_TITLE,
+        description: FATIGA_DESC,
+        img: Fatiga,
+    },
+    {
+        id: 2,
+        title: GESTION_TITLE,
+        description: GESTION_DESC,
+        img: Gestion,
+    },
+    {
+        id: 3,
+        title: MEMORIA_TITLE,
+        description: MEMORIA_DESC,
+        img: Memoria,
+    }
+];
