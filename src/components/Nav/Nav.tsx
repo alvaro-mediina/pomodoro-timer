689608e2c19d9a29c91bebdf6d50cd995fda309a
Alvaro Medina <alvaro.mediina2003@gmail.com>
2025-07-14 00:34:00 -0300

    refactor: :recycle: Refactorización del navbar | quitando innecesarios

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,14 @@
-import Logo from "./Logo";
-import MenuWhite from "../../assets/menu-white.svg";
-import MenuBlack from "../../assets/menu-black.svg";
+import Logo from './Logo';
 
-interface NavProps {
-  scrolledInfo: boolean;
-}
-
-const Nav = ({ scrolledInfo }: NavProps) => {
+function Nav() {
   return (
     <nav
-      className={`w-full h-20 flex flex-row justify-center items-center sm:text-xl z-50 transition-all duration-300 ${
-        scrolledInfo ? "bg-white text-black" : "bg-background text-white"
-      }`}
+      className="w-full h-20 flex flex-row justify-center items-center sm:text-xl z-50 
+      transition-all duration-300"
     >
-      <Logo className={scrolledInfo ? "text-black" : "text-white"} />
-
+      <Logo className="text-white" />
     </nav>
   );
-};
+}
 
 export default Nav;
