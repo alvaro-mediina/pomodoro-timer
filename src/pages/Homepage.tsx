3c1f4b82b734a13738a8df30597fa48229171f94
Alvaro Medina <alvaro.mediina2003@gmail.com>
2025-07-14 00:34:30 -0300

    refactor: :recycle: Quitando inecesarios

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,41 +1,13 @@
-import Nav from "../components/Nav/Nav";
-import Home from "../components/Home/Home";
-import Info from "../components/Info/Info";
-
-import { useState,useEffect } from "react";
+import Nav from '../components/Nav/Nav';
+import Home from '../components/Home/Home';
 
 function Homepage() {
-    const [scrolledInfo, setScrolledInfo] = useState(false);
-
-    // Manejo del cambio de color del Nav
-    useEffect(() => {
-        const scrollContainer = document.getElementById("scrollbar-hide") ||
-            document.querySelector(".homepage-container");
-        if (!scrollContainer) return;
-
-        const infoSection = document.getElementById("info-section");
-        if (!infoSection) return;
-
-        const infoTop = infoSection.offsetTop;
-
-        const handleScroll = () => {
-            // scrollY relativo al scrollContainer
-            const scrollY = scrollContainer.scrollTop;
-            setScrolledInfo(scrollY + 50 >= infoTop);
-        };
-
-        scrollContainer.addEventListener("scroll", handleScroll);
-        handleScroll(); // Estado inicial
-
-        return () => scrollContainer.removeEventListener("scroll", handleScroll);
-    }, []);
-
-    return (
+  return (
     <div className="bg-background flex flex-col min-h-screen">
-        <Nav scrolledInfo={scrolledInfo} />
-        <Home />
+      <Nav />
+      <Home />
     </div>
-    );
+  );
 }
 
 export default Homepage;
